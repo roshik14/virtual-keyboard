ff34e14d5c49a79bab280b402f497e493a95d87e
roshik14 <fraust1337@gmail.com>
2023-04-26 13:06:51 +0700

    fix: rename init func to App and change imports

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-/* eslint-disable import/extensions */
-import Header from './header/header.js';
-import Main from './main/main.js';
-import Footer from './footer/footer.js';
+import Header from './header/header';
+import Main from './main/main';
+import Footer from './footer/footer';
 
-const init = () => {
+const App = () => {
   const body = document.querySelector('body');
   body.append(Header('Virtual keyboard'));
   body.append(Main());
   body.append(Footer());
 };
 
-init();
+App();

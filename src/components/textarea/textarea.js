922e1c601166f900e3c7a14183d8f4cc6a9c2ff8
roshik14 <fraust1337@gmail.com>
2023-04-26 13:07:55 +0700

    fix: remove textarea.scss

--- src/components/textarea/textarea.js
+++ src/components/textarea/textarea.js
@@ -2,8 +2,9 @@ import './textarea.scss';
 
 const TextArea = () => {
   const textarea = document.createElement('textarea');
-  textarea.rows = 5;
+  textarea.rows = 10;
   textarea.classList.add('textarea');
+  textarea.autofocus = true;
   return textarea;
 };
 

f28383df9cad1130a53d8abf3071d4fb8add6983
roshik14 <fraust1337@gmail.com>
2023-04-26 13:10:17 +0700

    fix: remove getButtons function

--- src/main/main.js
+++ src/main/main.js
@@ -1,35 +1,12 @@
-/* eslint-disable import/extensions */
-import './main.scss';
-import TextArea from '../components/textarea/textarea.js';
-import Button from '../components/button/button.js';
-import Wrapper from '../components/wrapper/wrapper.js';
-import data from './buttons.json';
-
-const getButtons = () => {
-  const div = document.createElement('div');
-  data.forEach((x) => {
-    const content = x.writeable ? x.shiftKey : x.key;
-    const classList = ['button', 'button__offset'];
-    if (!x.writeable) {
-      classList.push('button__functional');
-    }
-    const button = Button({ content, classList });
-    div.append(button);
-  });
-  return div;
-};
-
-const getButtonsContainer = () => {
-  const container = getButtons();
-  container.classList.add('buttons');
-  return container;
-};
+import TextArea from '../components/textarea/textarea';
+import Wrapper from '../components/wrapper/wrapper';
+import Keyboard from '../components/keyboard/keyboard';
 
 const getContent = () => {
   const wrapper = Wrapper();
   const textArea = TextArea();
   wrapper.append(textArea);
-  wrapper.append(getButtonsContainer());
+  wrapper.append(Keyboard());
   return wrapper;
 };
 

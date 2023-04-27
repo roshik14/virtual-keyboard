bf3da5a2a1ef6c9ec91862f59509dea713ba79a9
roshik14 <fraust1337@gmail.com>
2023-04-27 17:53:27 +0700

    feat: add jsdoc

--- src/components/element/element.js
+++ src/components/element/element.js
@@ -1,9 +1,16 @@
+/**
+ * @param {string} element HTML tag of element
+ * @param {[string]} classList array of css classes that will be add to an element
+ * @param {string} content string that contains html content
+ * @param {{}} options Any valid properties of element
+ * @returns {HTMLElement}
+ */
 const Element = (element, classList = [], content = '', options = {}) => {
   const htmlElement = document.createElement(element);
   htmlElement.classList.add(...classList);
-  htmlElement.innerHTML = content;
+  htmlElement.insertAdjacentHTML('afterbegin', content);
   Object.keys(options).forEach((key) => {
-    htmlElement[key] = options[key];
+    htmlElement.setAttribute(key, options[key]);
   });
   return htmlElement;
 };

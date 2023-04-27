c7852a0999076600eaa8eb419cb8b4ab23bd20c5
roshik14 <fraust1337@gmail.com>
2023-04-27 17:53:02 +0700

    fix: add constants for html and css classes

--- src/main/main.js
+++ src/main/main.js
@@ -2,18 +2,28 @@ import Element from '../components/element/element';
 import TextArea from '../components/textarea/textarea';
 import Wrapper from '../components/wrapper/wrapper';
 import Keyboard from '../components/keyboard/keyboard';
+import './main.scss';
+
+const Html = {
+  MAIN: 'main',
+};
+
+const Css = {
+  MAIN: 'main',
+  MAIN_BG: 'main-bg',
+};
 
 const getContent = () => {
   const wrapper = Wrapper();
   const textarea = TextArea();
   const keyboard = new Keyboard();
   wrapper.append(textarea);
-  wrapper.append(keyboard.create());
+  wrapper.append(keyboard.getElement());
   return wrapper;
 };
 
 const Main = () => {
-  const main = Element('main');
+  const main = Element(Html.MAIN, [Css.MAIN, Css.MAIN_BG]);
   main.append(getContent());
   return main;
 };

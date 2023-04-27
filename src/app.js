7961d6792a4149a4ea5e77b83e32ae9e72cd2774
roshik14 <fraust1337@gmail.com>
2023-04-27 17:50:47 +0700

    feat: add html object

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import Header from './header/header';
 import Main from './main/main';
 import Footer from './footer/footer';
 
+const Html = {
+  BODY: 'body',
+};
+
 const App = () => {
-  const body = document.querySelector('body');
+  const body = document.querySelector(Html.BODY);
   body.append(Header('Virtual keyboard'));
   body.append(Main());
   body.append(Footer());

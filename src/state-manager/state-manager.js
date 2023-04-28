9b1ca71aa0c54ce7be66315a603b94a1c7a9ecd0
roshik14 <fraust1337@gmail.com>
2023-04-28 12:09:49 +0700

    feat: add correct delete and backspace button functional, move cursor to left and right and so on

--- src/state-manager/state-manager.js
+++ src/state-manager/state-manager.js
@@ -1,18 +1,66 @@
 /* eslint-disable no-console */
-// import Keyboard from '../components/keyboard/keyboard';
+import Keyboard from '../components/keyboard/keyboard';
 
-const getNewTextValue = (key, text) => {
-  console.log(key);
-  if (key.isWritable) {
-    return text + key.enKey;
+/**
+ * Determines event.code is Arrow key or not
+ * @param {String} code event.code of pressed key
+ * @returns {Boolean}
+ */
+
+const ArrowKey = {
+  UP: 'ArrowUp',
+  DOWN: 'ArrowDown',
+  LEFT: 'ArrowLeft',
+  RIGHT: 'ArrowRight',
+};
+
+const removeKeys = new Set([Keyboard.functionalKeys.BACKSPACE, Keyboard.functionalKeys.DELETE]);
+
+/**
+ * Add new key value to textarea value
+ * @param {{}} key Object represents key data
+ * @param {String} text Textarea value
+ * @param {Number} pos Cursor position
+ * @returns {String} new text value
+ */
+const addNewTextValue = (key, text, pos) => {
+  const newValue = key.code === Keyboard.functionalKeys.ENTER ? '\n' : key.enKey;
+  return `${text.substring(0, pos)}${newValue}${text.substring(pos)}`;
+};
+
+/**
+ * Removes previous or next character
+ * @param {{}} key Object represents key data
+ * @param {String} text Textarea value
+ * @param {Number} pos Cursor position
+ * @returns {String} new text value
+ */
+const removeTextValue = (key, text, pos) => {
+  if (key.code === Keyboard.functionalKeys.BACKSPACE) {
+    return text.substring(0, pos - 1) + text.substring(pos);
+  }
+  if (key.code === Keyboard.functionalKeys.DELETE) {
+    return text.substring(0, pos) + text.substring(pos + 1);
   }
   return text;
+};
+
+const moveCursorPosition = (key, pos) => {
+  if (key.code === ArrowKey.RIGHT) {
+    return pos + 1;
+  }
+  if (key.code === ArrowKey.LEFT) {
+    return !pos ? pos : pos - 1;
+  }
+  return pos;
 };
 
 class StateManager {
   #keyboard;
 
   #textarea;
+
+  #arrowKeys;
 
   /**
    * Construct StateManager that controls keyboard to manipulate textarea
@@ -22,17 +70,38 @@ class StateManager {
   constructor(keyboard, textarea) {
     this.#keyboard = keyboard;
     this.#textarea = textarea;
+    this.#arrowKeys = new Set(Object.values(ArrowKey));
     this.watch();
   }
 
   watch() {
     this.#keyboard.addEventListener('keydown', (event) => {
-      const key = this.#keyboard.getKey(event.code);
+      const key = this.#keyboard.getKey(event.code)?.data ?? null;
       if (key) {
         this.#textarea.focus();
-        this.#textarea.value = getNewTextValue(key.data, this.#textarea.value);
+        const cursorPos = this.#textarea.selectionStart;
+        if (key.isWritable) {
+          this.#textarea.value = addNewTextValue(key, this.#textarea.value, cursorPos);
+          this.updateCursorPosition(cursorPos + 1);
+        }
+        if (removeKeys.has(key.code)) {
+          this.#textarea.value = removeTextValue(key, this.#textarea.value, cursorPos);
+          const pos = key.code === Keyboard.functionalKeys.BACKSPACE && cursorPos
+            ? cursorPos - 1
+            : cursorPos;
+          this.updateCursorPosition(pos);
+        }
+        if (this.#arrowKeys.has(key.code)) {
+          const newPos = moveCursorPosition(key, cursorPos);
+          this.updateCursorPosition(newPos);
+        }
       }
     });
+  }
+
+  updateCursorPosition(pos) {
+    this.#textarea.selectionStart = pos;
+    this.#textarea.selectionEnd = pos;
   }
 }
 

5f85518256a461bc62011152a29b757559a0ea20
roshik14 <fraust1337@gmail.com>
2023-05-01 10:59:34 +0700

    feat: add keyboard description

--- src/main/main.js
+++ src/main/main.js
@@ -7,16 +7,25 @@ import './main.scss';
 
 const Html = {
   MAIN: 'main',
+  DIV: 'div',
+  Paragraph: 'p',
 };
 
 const Css = {
   MAIN: 'main',
   MAIN_BG: 'main-bg',
+  DESCRIPTION: 'description',
 };
 
 const startListen = (keyboard, textarea) => {
   const stateManager = new StateManager(keyboard, textarea);
   stateManager.watch();
+};
+
+const getSwitchLangDescription = () => {
+  const content = 'Keyboard was made on Linux OS (Ubuntu). To switch language press Ctrl + Alt';
+  const paragraph = Element(Html.Paragraph, [Css.DESCRIPTION], content);
+  return paragraph;
 };
 
 const getContent = () => {
@@ -26,6 +35,7 @@ const getContent = () => {
   startListen(keyboard, textarea);
   wrapper.append(textarea);
   wrapper.append(keyboard.getElement());
+  wrapper.append(getSwitchLangDescription());
   return wrapper;
 };
 
